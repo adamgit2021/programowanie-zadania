3ab3fdf2df5662b6fbe8375771dca6f70e91d1e5
adamgit2021 <93943655+adamgit2021@users.noreply.github.com>
2021-11-08 20:05:53 +0100

    Add files via upload

---
+++ zadanie 10/zadanie 10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace zadanie_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę:");   //wyświetla napis - Podaj pierwszą liczbę:
+            int a = int.Parse(Console.ReadLine());  //konwertuje zmienną string na int
+
+            Console.WriteLine("Podaj drugą liczbę:");   //wyświetla napis - Podaj drugą liczbę:
+            int b = int.Parse(Console.ReadLine());  //konwertuje zmienną string na int
+
+            Console.WriteLine("Wynik: " + a + '+' + b + '=' + (a + b));  //wyświetla - Wynik:
+
+            Console.WriteLine();    //pusta linijka
+
+            Console.WriteLine("Podaj trzecią liczbę:");   //wyświetla napis - Podaj trzecią liczbę:
+            int c = int.Parse(Console.ReadLine());  //konwertuje zmienną string na int
+
+            //mnoży sume (a+b) razy c i przypisuje wynik do c
+            Console.WriteLine("Wynik: (" + a + '+' + b + ')' + '*' + c + '='  + (c *= (a + b))); 
+
+            Console.WriteLine();    //pusta linijka
+
+            Console.WriteLine("Podaj czwartą liczbę:");   //wyświetla napis - Podaj czwartą liczbę:
+            int d = int.Parse(Console.ReadLine());  //konwertuje zmienną string na int
+
+            //wyświetla wynik dzielenia modulo poprzedniego wyniku i przypisuje wartość do zmiennej d
+            Console.WriteLine("Wynik: ((" + a + '+' + b + ')' + '*' + c / (a + b) + ')' + '%' + d + '=' + (d %= c));
+
+            Console.ReadKey();
+        }
+    }
+}
