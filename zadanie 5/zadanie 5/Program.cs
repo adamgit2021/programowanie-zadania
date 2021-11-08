812fa89d99ad924b01143513448ead46d6fa7ebc
adamgit2021 <93943655+adamgit2021@users.noreply.github.com>
2021-11-08 20:03:40 +0100

    Add files via upload

---
+++ zadanie 5/zadanie 5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zadanie_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę większą od 1:");    //wyświetla napis - Podaj liczbę większą od 1:
+            double a = double.Parse(Console.ReadLine());        //konwertuje zmienną string na double
+            int b = (int)a;                                     //konwertuje zmienną double na int
+             
+            Console.WriteLine("wynik 1 =" + a);                 //wyświetla wprowadzoną liczbę
+            Console.WriteLine("wynik 2 =" + b);                 //wyświetla liczbę skonwertowaną z double do int
+            Console.ReadKey();
+        }
+    }
+}
