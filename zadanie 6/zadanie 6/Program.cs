04380de61d4d572d11e1ca077334703716297d5b
adamgit2021 <93943655+adamgit2021@users.noreply.github.com>
2021-11-08 20:04:17 +0100

    Add files via upload

---
+++ zadanie 6/zadanie 6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zadanie_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Console.WriteLine("Podaj pierwszą liczbę:");    //wyświetla napis - Podaj pierwszą liczbę:
+            int a = int.Parse(Console.ReadLine());  //konwertuje zmienną string na int
+
+            Console.WriteLine("Podaj drugą liczbę:");   //wyświetla napis - Podaj drugą liczbę:
+            int b = int.Parse(Console.ReadLine());  //konwertuje zmienną string na int
+
+            Console.WriteLine("wynik dodawania liczby a i b = " + (a+b));  //wynik dodawania liczby a i b
+            Console.WriteLine("wynik mnożenia liczby a i b = " + a*b);   //wynik mnożenia liczby a i b
+            Console.WriteLine("wynik modulo = " + a%b);  //wynik modulo
+
+            Console.ReadKey();
+        }
+    }
+}
