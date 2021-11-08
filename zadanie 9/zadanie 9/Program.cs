327e1add9915bc62575bd07202e5711f28462ac3
adamgit2021 <93943655+adamgit2021@users.noreply.github.com>
2021-11-08 20:05:24 +0100

    Add files via upload

---
+++ zadanie 9/zadanie 9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace zadanie_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę:");   //wyświetla napis - Podaj pierwszą liczbę:
+            int a = int.Parse(Console.ReadLine());  //konwertuje zmienną string na int
+
+            Console.WriteLine("Podaj drugą liczbę:");   //wyświetla napis - Podaj drugą liczbę:
+            int b = int.Parse(Console.ReadLine());  //konwertuje zmienną string na int
+
+            if(!(a+b<15) || a*b>=60)
+            {
+                Console.WriteLine("Liczby " + a + " i " + b + " spełniają warunek");
+                //wyświetla napis dla spełnionych warunków
+            }
+            else
+            {
+                Console.WriteLine("Liczby " + a + " i " + b + " nie spełniają warunku");
+                //wyświetls napis dla niespełnionego/niespełnionych warunków
+            }
+            Console.ReadKey();
+        }
+    }
+}
