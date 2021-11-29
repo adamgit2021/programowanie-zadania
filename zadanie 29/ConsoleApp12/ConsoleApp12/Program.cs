bd40aec1570ecabaf44003eec465a7338a2a8cd7
adamgit2021 <93943655+adamgit2021@users.noreply.github.com>
2021-11-29 13:04:31 +0100

    Add files via upload

---
+++ zadanie 29/ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //zadanie 29
+            Console.WriteLine("Podaj pierwszą liczbę");
+            int liczba1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugą liczbę");
+            int liczba2 = Int32.Parse(Console.ReadLine());
+
+            bool max1; //pierwsza liczba jest większa
+
+            if (liczba1 > liczba2)
+            {
+                max1 = true;
+                Console.WriteLine("Max: " + liczba1);
+            }
+            else
+            {
+                max1 = false;
+                Console.WriteLine("Max: " + liczba2);
+            }
+
+        }
+    }
+}
