6a0586d279265fea0150b1b888b94439f3e760a7
adamgit2021 <93943655+adamgit2021@users.noreply.github.com>
2022-02-01 20:15:08 +0100

    Add files via upload

---
+++ QUIZv2/QUIZv2/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace QUIZv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Pytanie = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(384, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SprawdźOdpowiedź);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(415, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(384, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.SprawdźOdpowiedź);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 445);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(384, 44);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SprawdźOdpowiedź);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(415, 445);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(384, 44);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.SprawdźOdpowiedź);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QUIZv2.Properties.Resources.woda;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(741, 235);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Pytanie
+            // 
+            this.Pytanie.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Pytanie.Location = new System.Drawing.Point(39, 291);
+            this.Pytanie.Name = "Pytanie";
+            this.Pytanie.Size = new System.Drawing.Size(741, 63);
+            this.Pytanie.TabIndex = 5;
+            this.Pytanie.Text = "Pytanie";
+            this.Pytanie.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 501);
+            this.Controls.Add(this.Pytanie);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quiz Ptaki polskie";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Pytanie;
+    }
+}
+
---
+++ QUIZv2/QUIZv2/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUIZv2
+{
+    public partial class Form1 : Form
+    {
+        // quiz zmienne
+        int prawidłowaOdpowiedź;
+        int numerPytania = 1;
+        int wynik;
+        int procent;
+        int wszystkiePytania;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ZadajPytanie(numerPytania);
+
+            wszystkiePytania = 10;
+        }
+
+        private void Pytanie_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SprawdźOdpowiedź(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == prawidłowaOdpowiedź)
+            {
+                wynik++;
+            }
+
+            if (numerPytania == wszystkiePytania)
+            {
+                // work out the percentage
+
+                procent = (int)Math.Round((double)(wynik * 100) / wszystkiePytania);
+
+                MessageBox.Show(
+                    "GRATULACJE!" + Environment.NewLine +
+                    "Odpowiedziałeś/aś na " + wynik + " pytań prawidłowo," + Environment.NewLine +
+                    "co stanowi " + procent + "% pytań." + Environment.NewLine +
+                    "Naciśnij OK, aby zagrać ponownie"
+                    );
+                wynik = 0;
+                numerPytania = 0;
+                ZadajPytanie(numerPytania);
+            }
+            numerPytania++;
+            ZadajPytanie(numerPytania);
+        }
+
+        private void ZadajPytanie(int qnum)
+        {
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.mazurek;
+                    Pytanie.Text = "Ptak widoczny na zdjęciu to?";
+
+                    button1.Text = "drzemlik";
+                    button2.Text = "wróbel";
+                    button3.Text = "mazurek";
+                    button4.Text = "dzięcioł";
+
+                    prawidłowaOdpowiedź = 3;
+
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources.trznadel;
+                    Pytanie.Text = "Jak nazwa się ten ptak?";
+
+                    button1.Text = "trznadel";
+                    button2.Text = "mysikrólik";
+                    button3.Text = "lodówka";
+                    button4.Text = "dudek";
+
+                    prawidłowaOdpowiedź = 1;
+
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Properties.Resources.kaktus;
+                    Pytanie.Text = "Największa sowa w polsce to?";
+
+                    button1.Text = "sowa śnieżna";
+                    button2.Text = "płomykówka";
+                    button3.Text = "puszczyk";
+                    button4.Text = "puchacz";
+
+                    prawidłowaOdpowiedź = 4;
+
+                    break;
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources.question;
+                    Pytanie.Text = "Jaki jest najmniejszy ptak w Polsce?";
+
+                    button1.Text = "zniczek";
+                    button2.Text = "wróbel";
+                    button3.Text = "mysikrólik";
+                    button4.Text = "zimorodek";
+
+                    prawidłowaOdpowiedź = 3;
+
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.waga;
+                    Pytanie.Text = "Największy ptak w Polsce pod względem ciężaru to?";
+
+                    button1.Text = "kormoran";
+                    button2.Text = "czapla";
+                    button3.Text = "łabędź niemy";
+                    button4.Text = "słowik";
+
+                    prawidłowaOdpowiedź = 3;
+
+                    break;
+
+                case 6:
+                    pictureBox1.Image = Properties.Resources.skrzydla;
+                    Pytanie.Text = "Jak nazywa się największy ptak w Polsce" + Environment.NewLine +
+                        "pod względem rozpiętości skrzydeł?";
+
+                    button1.Text = "flaming";
+                    button2.Text = "bielik";
+                    button3.Text = "sęp";
+                    button4.Text = "orzeł przedni";
+
+                    prawidłowaOdpowiedź = 2;
+
+                    break;
+
+                case 7:
+                    pictureBox1.Image = Properties.Resources.nuty;
+                    Pytanie.Text = "Największy polski ptak śpiewający to?";
+
+                    button1.Text = "sójka";
+                    button2.Text = "harpia wielka";
+                    button3.Text = "skowronek";
+                    button4.Text = "kruk";
+
+                    prawidłowaOdpowiedź = 4;
+
+                    break;
+
+                case 8:
+                    pictureBox1.Image = Properties.Resources.woda;
+                    Pytanie.Text = "Jak nazywa się jedyny polski ptak śpiewający," + Environment.NewLine +
+                        "który potrafi dobrze nurkować i pływać?";
+
+                    button1.Text = "pluszcz zwyczajny";
+                    button2.Text = "pingwin białobrewy";
+                    button3.Text = "zimorodek";
+                    button4.Text = "perkoz";
+
+                    prawidłowaOdpowiedź = 1;
+
+                    break;
+
+                case 9:
+                    pictureBox1.Image = Properties.Resources.question;
+                    Pytanie.Text = "Który z tych ptaków nie jest chroniony?";
+
+                    button1.Text = "pójdźka";
+                    button2.Text = "bażant";
+                    button3.Text = "cietrzew";
+                    button4.Text = "jemiołuszka";
+
+                    prawidłowaOdpowiedź = 2;
+
+                    break;
+
+                case 10:
+                    pictureBox1.Image = Properties.Resources.limby;
+                    Pytanie.Text = "Ptak, który omyłkowo sieje sosny limby to?";
+                    button1.Text = "kopciuszek";
+                    button2.Text = "gil";
+                    button3.Text = "biegus rdzawy";
+                    button4.Text = "orzechówka zwyczajna";
+
+                    prawidłowaOdpowiedź = 4;
+
+                    break;
+            }
+        }
+    }
+}
